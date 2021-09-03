f1e869624b5402607f2cb2f9da815aa57cfcce06
Javier Guerra <javier.j.guerra4@gmail.com>
2021-09-03 15:35:33 -0500

    Exercises

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -1,6 +1,62 @@
+
+import java.util.Scanner;
+
 public class ConsoleExercises {
     public static void main(String[] args) {
-        double pi = 3.14159;
-        System.out.printf("Formatted with precision: pi = %.2f\n", Math.PI);
+//        Exercise #1
+//        double pi = 3.14159;
+//        System.out.printf("Formatted with precision: pi = %.2f\n", Math.PI);
+
+
+//        Exercise #1 Instructor solution
+//        double pi = 3.14159;
+//        System.out.printf("The value of pi is approximately %.2f.%n", pi);
+
+//        Exercise #2
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.print("Enter an integer: ");
+//        int nextInt() = scanner.next();
+//        System.out.println("You entered: " + );
+
+//        Instructor  3************
+        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Enter an integer: ");
+//        int userInput = scanner.nextInt();
+//        System.out.println(userInput);
+
+
+//        Instructor 4************
+//        System.out.println("Enter a word: ");
+//        String word1 = scanner.nextLine();
+//        System.out.println("Enter a word: ");
+//        String word2 = scanner.nextLine();
+//        System.out.println("Enter a word: ");
+//        String word3 = scanner.nextLine();
+
+
+//        System.out.println("Enter three words: ");
+//        String word1 = scanner.next();
+//        String word2 = scanner.next();
+//        String word3 = scanner.next();
+//
+//        System.out.printf("Your three words were: %n%s%n%s%n%s. ", word1, word2, word3);
+
+        //        Instructor 5************
+
+//        System.out.println("Please enter a sentence: ");
+//        String sentence = scanner.nextLine();
+//        System.out.println(sentence);
+
+
+
+//        Instructor ************
+        System.out.println("Please enter the length of your classroom:");
+        int length = Integer.parseInt(scanner.nextLine());
+        System.out.println("Please enter the width of your classroom:");
+        int width = Integer.parseInt(scanner.nextLine());
+        System.out.println("The area of your room is " + (width * length) + " and the parimeter is " + (2 * (widthInFeet )));
+
+
+
     }
 }
