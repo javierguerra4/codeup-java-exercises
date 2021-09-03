78e68a3d41e813ad92dd8b2f5c7307c3ad34d6f5
Javier Guerra <javier.j.guerra4@gmail.com>
2021-09-03 14:29:39 -0500

    Exercise 1 done

--- src/Test.java
+++ src/Test.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Hello, Draco!");
+//        System.out.print("Hello, Dracoooooooo!");
+
+//        Scanner scanner = new Scanner(System.in);
+//            System.out.println ("What is your name?:");
+//            String userInput = scanner.nextLine();
+//            System.out.println("Hello " + userInput + "!");
+
+        Scanner scanner = new Scanner(System.in);
+//            System.out.println ("What is your name?:");
+//            String userInput = scanner.nextLine();
+//            System.out.println("Hello " + userInput + "!");
+//        System.out.printf("Hello %s" userInput );
+        /*integer system output*/
+        System.out.println("Enter an integer: ");
+        int userInput = scanner.nextInt();
+        System.out.printf("You entered %d%n", userInput);
+//
+
     }
 }
