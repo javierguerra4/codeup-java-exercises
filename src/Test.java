1a322f0dde7dfac2914f846f2936be5afed620e7
Javier Guerra <javier.j.guerra4@gmail.com>
2021-09-06 13:04:17 -0500

    Few exercises left

--- src/Test.java
+++ src/Test.java
@@ -2,14 +2,38 @@ import java.util.Scanner;
 
 public class Test {
     public static void main(String[] args) {
-        int myFavoriteNumber = 82;
-        System.out.println(myFavoriteNumber);
+//        int myFavoriteNumber = 82;
+//        System.out.println(myFavoriteNumber);
+//
+//        String myString = "Go Cowboys!";
+//        System.out.println(myString);
+//
+//        float myNumber = 3.14;
+//        System.out.println(myNumber);
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
 
-        String myString = "Go Cowboys!";
-        System.out.println(myString);
+//        int x = 5;
+//        System.out.println(++x);
+////        System.out.println(x);
 
-        long myNumber = 123;
-        System.out.println(myNumber);
+//        int x = 4;
+//        x += 5;
+//        System.out.println(x);
+
+
+//        int x = 3;
+//        int y = 4;
+//        y *= x;
+//        System.out.println(y);
+
+        int x = 10;
+        int y = 2;
+        x = x / y;
+        y = y - x;
+
+
 //        System.out.print("Hello, Dracoooooooo!");
 
 //        Scanner scanner = new Scanner(System.in);
