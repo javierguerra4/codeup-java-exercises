d88799c15ebd3d93c95160474f1cf99c91dbcee1
Javier Guerra <javier.j.guerra4@gmail.com>
2021-09-07 13:52:47 -0500

    FizzBuzz done

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -4,6 +4,7 @@ public class ControlFlowExercises {
 //while( i <= 15){
 //    System.out.println(i);
 //    i++;
+//        **************Count by 2 backwards***********
 
 //        int i = 0;
 //        do {
@@ -13,14 +14,49 @@ public class ControlFlowExercises {
 //        } while (i < 100);
 //        System.out.println("This is the last number in the loop" + " " + i);
 
-        int i = 100;
-        System.out.println("We're starting our loop with" + " " + i + "." + "Now let's start our loop.");
-        do {
-            i = i - 5;
-            System.out.println("This is the next number in the loop,"+ " " + i);
+//        **************Count by 5 backwards***********
+//        int i = 100;
+//        System.out.println("We're starting our loop with" + " " + i + "." + "Now let's start our loop.");
+//        do {
+//            i = i - 5;
+//            System.out.println("This is the next number in the loop,"+ " " + i);
+//
+//        } while (i > -10);
+//        System.out.println("This is the last number in the loop," + " " + i);
 
-        } while (i > -10);
-        System.out.println("This is the last number in the loop," + " " + i);
+//        **************Count by 2 squared***********
+
+//        int i = 2;
+//        System.out.println("We're starting our loop with" + " " + i + "." + "Now let's start our loop.");
+//        do {
+//            i = i * i;
+//            System.out.println("This is the next number in the loop,"+ " " + i);
+//
+//        } while (i < 1000000);
+//        System.out.println("This is the last number in the loop," + " " + i);
+
+//     *************FizzBuzz***************
+//        int i = 0;
+//        while (i <= 100) {
+//            i++;
+//            if ( i / 3 == 0){
+//                System.out.println("Fizz");
+//            System.out.println("i is " + i );
+//            i++;
+//            if ( i / 3 == 0){
+//            System.out.println("Fizz");
+        for (int i = 1; i <= 100; i++){
+            if (((i % 3) == 0) && ((i % 5) == 0 ))
+                System.out.println("FizzBuzz");
+                else if ((i % 3) == 0)
+                    System.out.println("Fizz");
+                else if ((i % 5) == 0)
+                    System.out.println("Buzz");
+                else
+                    System.out.println(i);
+        }
+
+
     }
 }
 
