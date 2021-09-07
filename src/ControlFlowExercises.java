7ca426de9bb889697a649613ca0858636b738c02
Javier Guerra <javier.j.guerra4@gmail.com>
2021-09-07 11:20:34 -0500

    Do while count by 2 done

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,14 +1,18 @@
 public class ControlFlowExercises {
     public static void main(String[] args) {
-        int i = 5;
-while( i <= 15){
-    System.out.println(i);
-    i++;
-}
+//        int i = 5;
+//while( i <= 15){
+//    System.out.println(i);
+//    i++;
 
+        int i = 0;
+        do {
+            i = i + 2;
+            System.out.println("This is the next number in the loop"+ " " + i);
 
+        } while (i < 100);
+        System.out.println("This is the last number in the loop" + " " + i);
     }
 }
-
 
 //    Create a while loop that runs so long as i is less than or equal to 15
