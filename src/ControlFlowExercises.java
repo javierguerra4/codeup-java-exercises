f6da6da0295721ee6d3fd7fd6838534eca462171
Javier Guerra <javier.j.guerra4@gmail.com>
2021-09-07 11:31:26 -0500

    Count by 5 backwards

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -5,13 +5,22 @@ public class ControlFlowExercises {
 //    System.out.println(i);
 //    i++;
 
-        int i = 0;
-        do {
-            i = i + 2;
-            System.out.println("This is the next number in the loop"+ " " + i);
+//        int i = 0;
+//        do {
+//            i = i + 2;
+//            System.out.println("This is the next number in the loop"+ " " + i);
+//
+//        } while (i < 100);
+//        System.out.println("This is the last number in the loop" + " " + i);
 
-        } while (i < 100);
-        System.out.println("This is the last number in the loop" + " " + i);
+        int i = 100;
+        System.out.println("We're starting our loop with" + " " + i + "." + "Now let's start our loop.");
+        do {
+            i = i - 5;
+            System.out.println("This is the next number in the loop,"+ " " + i);
+
+        } while (i > -10);
+        System.out.println("This is the last number in the loop," + " " + i);
     }
 }
 
