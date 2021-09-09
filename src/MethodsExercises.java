6c19cdd4b5421f6ab8b7c36b17768168393371df
Javier Guerra <javier.j.guerra4@gmail.com>
2021-09-09 14:43:14 -0500

    Exercise 1 nearly complete. Need answer for modulus if 0

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -6,28 +6,32 @@ public class MethodsExercises {
 ////        multiply(10, 5);
 ////        division(10, 5);
 //    }
-    public static void addition(int a, int b){
-        System.out.println(a + b);
+    public static int addition(int a, int b){
+        if (a > b) return a + b;
+        return b + a;
+//        System.out.println(a + b);
     }
-    public static void subtraction(int a, int b){
-        System.out.println(a - b);
-//        if (a > b) return a - b;
-//        return b - a;
+    public static int subtraction(int a, int b){
+        if (a > b) return a - b;
+        return b - a;
     }
-    public static void multiply(int a, int b){
-        System.out.println(a * b);
+    public static int multiply(int a, int b){
+        if (a > b) return a * b;
+        return b * a;
     }
-    public static void division(int a, int b){
-        System.out.println(a / b);
+    public static int division(int a, int b){
+        if (a > b) return a / b;
+        return b / a;
     }
-    public static void modulus(int a, int b){
-        System.out.println(a % b);
+    public static int modulus(int a, int b){
+        if (a > b) return a % b;
+        return b % a;
     }
     public static void main(String[] args) {
-        addition (10, 5);
-       subtraction(5, 10);
-        multiply(10, 5);
-        division(10, 5);
-        modulus(20, 3);
+        System.out.println(addition(5, 10));
+        System.out.println(subtraction(5, 100));
+        System.out.println(multiply(10, 5));
+        System.out.println(division(10, 5));
+        System.out.println(modulus(20, 5));
     }
 }
