0fa6d729f1b437a9c6f16ff4e5cdf3fcefd6198e
Javier Guerra <javier.j.guerra4@gmail.com>
2021-09-09 14:29:09 -0500

    Basic arithmetic lesson almost complete

--- src/MethodWarmup.java
+++ src/MethodWarmup.java
@@ -1,12 +1,45 @@
-public class MethodWarmup {
-
-
-    public static int getDifference(int x, int y) {
-
-        return x - y;
-    }
-    public static void main(String[] args) {
-int z = getDifference(8, 3);
-        System.out.println(z);
-    }
-}
+//public class MethodWarmup {
+//
+//
+////    public static int getDifference(int x, int y) {
+////
+////        return (x - y);
+////    }
+////    public static void main(String[] args) {
+////int z = getDifference(8, 3);
+////        System.out.println(z);
+////    }
+//
+//
+//
+//
+//    // TODO: use recursion to add all numbers up, from 1 to the given number input
+//    /*
+//        addNums(5) would trigger a calculation of...
+//            5 + 4 + 3 + 2 + 1, evaluating to 15
+//     */
+//    // public static int addNums(int num) {
+//    // }
+//
+//
+//    public static int addNums(int n)
+//    {
+//        if (n <= 1)
+//            return n;
+//        return n + addNums(n - 1);
+//    }
+//
+//    // Driver code
+//    public static void main(String args[])
+//    {
+//        int n = 8;
+//        System.out.println(addNums(n));
+//    }
+//
+////    Instruc solutino
+////    public static int addNums(int num) {
+////        if (num == 1) return 1;
+////        return addNums(num - 1) + num;
+////    }
+//
+//}
---
+++ src/MethodsExercises.java
@@ -0,0 +1,33 @@
+public class MethodsExercises {
+
+//    public static void main(String[] args) {
+//        addition (10, 5);
+//        return subtraction(10, 5);
+////        multiply(10, 5);
+////        division(10, 5);
+//    }
+    public static void addition(int a, int b){
+        System.out.println(a + b);
+    }
+    public static void subtraction(int a, int b){
+        System.out.println(a - b);
+//        if (a > b) return a - b;
+//        return b - a;
+    }
+    public static void multiply(int a, int b){
+        System.out.println(a * b);
+    }
+    public static void division(int a, int b){
+        System.out.println(a / b);
+    }
+    public static void modulus(int a, int b){
+        System.out.println(a % b);
+    }
+    public static void main(String[] args) {
+        addition (10, 5);
+       subtraction(5, 10);
+        multiply(10, 5);
+        division(10, 5);
+        modulus(20, 3);
+    }
+}
