427a2ffdb8c9fd6c796e93d1e6a709490d144f05
Javier Guerra <javier.j.guerra4@gmail.com>
2021-09-09 16:26:18 -0500

    Finished exercise 2

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -1,37 +1,50 @@
-public class MethodsExercises {
+import java.util.Scanner;
 
-//    public static void main(String[] args) {
-//        addition (10, 5);
-//        return subtraction(10, 5);
-////        multiply(10, 5);
-////        division(10, 5);
+public class MethodsExercises {
+//
+////*****************Exercise 1**************
+//    public static int addition(int a, int b){
+//        if (a > b) return a + b;
+//        return b + a;
+////        System.out.println(a + b);
 //    }
-    public static int addition(int a, int b){
-        if (a > b) return a + b;
-        return b + a;
-//        System.out.println(a + b);
-    }
-    public static int subtraction(int a, int b){
-        if (a > b) return a - b;
-        return b - a;
-    }
-    public static int multiply(int a, int b){
-        if (a > b) return a * b;
-        return b * a;
-    }
-    public static int division(int a, int b){
-        if (a > b) return a / b;
-        return b / a;
-    }
-    public static int modulus(int a, int b){
-        if (a > b) return a % b;
-        return b % a;
-    }
+//    public static int subtraction(int a, int b){
+//        if (a > b) return a - b;
+//        return b - a;
+//    }
+//    public static int multiply(int a, int b){
+//        if (a > b) return a * b;
+//        return b * a;
+//    }
+//    public static int division(int a, int b){
+//        if (a > b) return a / b;
+//        return b / a;
+//    }
+//    public static int modulus(int a, int b){
+//        if (a > b) return a % b;
+//        return b % a;
+//    }
+//    public static void main(String[] args) {
+//        System.out.println(addition(5, 10));
+//        System.out.println(subtraction(5, 100));
+//        System.out.println(multiply(10, 5));
+//        System.out.println(division(10, 5));
+//        System.out.println(modulus(20, 21));
+//    }
+
+
+    //    ************Exercise 2************
     public static void main(String[] args) {
-        System.out.println(addition(5, 10));
-        System.out.println(subtraction(5, 100));
-        System.out.println(multiply(10, 5));
-        System.out.println(division(10, 5));
-        System.out.println(modulus(20, 5));
+        int userInput = getInteger(1, 10);
+        System.out.printf("You entered %d \n", userInput);
+    }
+    public static int getInteger(int min, int max) {
+        int input;
+        Scanner in = new Scanner(System.in);
+        do {
+            System.out.print("Enter a value between " + min + " and " + max + ": ");
+            input = in.nextInt();
+        } while (input < min || input > max);
+        return input;
     }
 }
