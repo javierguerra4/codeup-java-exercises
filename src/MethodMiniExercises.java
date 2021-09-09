f7a2d2809b9ad4e19f991de5abf66ef42e3ce3d2
Javier Guerra <javier.j.guerra4@gmail.com>
2021-09-09 09:27:05 -0500

    Method is subtracting both integers given

---
+++ src/MethodMiniExercises.java
@@ -0,0 +1,29 @@
+//import java.util.Scanner;
+//
+//public class MethodMiniExercises {
+//
+//    // TODO: break the following code apart into a few methods
+//
+//    public static void input (String someInput){
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Please enter an input.");
+//    }
+//
+//
+//
+//
+//    public static void main(String[] args) {
+////        Scanner sc = new Scanner(System.in);
+////        System.out.println("Please enter an input.");
+//
+//        String stringInput = sc.nextLine();
+//        System.out.println("Please enter an integer.");
+//        int intInput = sc.nextInt(); // assume the user will enter a valid integer
+//        if (stringInput.length() == intInput) {
+//            System.out.println("That matches the length of the input!");
+//        } else {
+//            System.out.println("That doesn't match the length of the input!");
+//        }
+//    }
+//
+//}
---
+++ src/MethodWarmup.java
@@ -0,0 +1,12 @@
+public class MethodWarmup {
+
+
+    public static int getDifference(int x, int y) {
+
+        return x - y;
+    }
+    public static void main(String[] args) {
+int z = getDifference(8, 3);
+        System.out.println(z);
+    }
+}
