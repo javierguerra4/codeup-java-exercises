37c4797eceef0c600d481c9bb80359232fddb434
Javier Guerra <javier.j.guerra4@gmail.com>
2021-09-20 13:32:44 -0500

    Collections started

---
+++ src/CollectionsTest.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+
+public class CollectionsTest {
+    public static void main(String[] args) {
+        /*
+    Create a class called CollectionsTest and include the following...
+    TODO: create a static method, omit2s, that takes in an array list of integers.
+      The method should return an arraylist of integers with the same values as the
+      input arraylist but without 2s.
+ */
+
+        public static omit2s(){
+            ArrayList<Integer> numbers = new ArrayList<>();
+
+        };
+
+    }
+}
---
+++ src/PrimitiveAndReference.java
@@ -0,0 +1,34 @@
+public class PrimitiveAndReference {
+
+    public static void main(String[] args) {
+
+
+//Example type and name
+        int count;
+
+
+//     Primitive types
+        boolean
+        char
+        byte
+        short
+        int
+        long
+        float
+        double
+
+//      Example code
+
+        boolean tacosDelicious = true;
+        long big = 3456789;
+        float f = 32.5f;
+
+
+        myDog.bark();
+
+        Dog d = newDog();
+        d.bark();
+
+
+    }
+}
