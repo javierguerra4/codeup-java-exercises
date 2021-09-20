ef88991dd751e8b64e8643bbbb1c213d29cfb452
Javier Guerra <javier.j.guerra4@gmail.com>
2021-09-20 16:44:22 -0500

    Created loop and average

--- src/grades/Student.java
+++ src/grades/Student.java
@@ -22,12 +22,44 @@ public class Student {
     public void setName(String name) {
         this.name = name;
     }
+//************
 
-    public void addGrade(int studentGrade){
+    public void addGrade(int studentGrade) {
         grades.add(studentGrade);
+    }
 
+        public String getGrades() {
+            String newList = "";
+            for (Integer x : this.grades) {
+                if (this.grades.indexOf(x) != this.grades.size() - 1) {
+                    newList += x + ", ";
+                } else {
+                    newList += x;
+                }
+            }
+            return newList;
+        }
+
+
+    public double getGradeAverage() {
+        double totalGrades = 0;
+        int length = 0;
+        for (Integer x : this.grades) {
+            totalGrades = totalGrades + x;
+            length += 1;
+        }
+        return totalGrades / length;
+    }
+
+
+    public static void main(String[] args) {
 
     }
+
+
+
+
+
 
 
 
