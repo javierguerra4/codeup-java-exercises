f90f13fe84bb39992b66f98019225acba859df49
Javier Guerra <javier.j.guerra4@gmail.com>
2021-10-15 09:32:54 -0500

    Working on returnTotalDifference warmup

--- src/JavaDrills.java
+++ src/JavaDrills.java
@@ -1,3 +1,6 @@
+import java.util.Arrays;
+import java.util.List;
+
 public class JavaDrills {
 //    Working progress
     public static String flipOuterCase(String text)
@@ -17,8 +20,42 @@ public class JavaDrills {
         }
         return new String(chars);
     }
+//**********************
+//    In `codeup-java-exercises`, in `JavaDrills`:
+//    Create a static method, returnTotalDifference, that takes in two lists of integers and returns the difference between the sum of all integers in the first list minus the sum of all integers in the second list.
+//    returnTotalDifference(Arrays.asList(10, 2, 3), Arrays.asList(1, 2, 3)); // returns 9
+//    returnTotalDifference(Arrays.asList(10, 1), Arrays.asList(1, 7)); // returns 3
+//    returnTotalDifference(Arrays.asList(10, 1), Arrays.asList(1, 7, 1)); // returns 2
 
-    public static void main(String input){
-        System.out.println("tEsT.flipOuterCase");
+//    Working progress************
+public static int returnTotalDifference(List<Integer> listOne, List<Integer> listTwo) {
+    int sum = 0;
+    for (int i : listOne) {
+        sum += i;
     }
+    return sum;
+}
+List<Integer> ints = Arrays.asList(1, 2, 3);
+int sum = MathUtils.sum(ints);
+
+};
+
+
+
+
+
+
+
+
+    public static void main(String[] args){
+        System.out.println(JavaDrills.flipOuterCase("CaT"));
+    }
+
+
+
+
+
+
+
+
 }
--- src/MovieWarmUp.java
+++ src/MovieWarmUp.java
@@ -1,39 +1,39 @@
-public class MovieWarmUp implements Entertains{
-
-    private String name;
-    private String director;
-
-
-//    The Getters And Setters for the instance variables.
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDirector() {
-        return director;
-    }
-
-    public void setDirector(String director) {
-        this.director = director;
-    }
-
-//    This is the constructor.
-    public MovieWarmUp(String name, String director)throws IllegalArgumentException {
-        if (name == null || name.isBlank() || director == null || director.isBlank()){
-        throw new IllegalArgumentException(); }
-        this.name = name;
-        this.director = director;
-    }
+//public class MovieWarmUp implements Entertains{
+//
+//    private String name;
+//    private String director;
+//
+//
+////    The Getters And Setters for the instance variables.
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//
+//    public String getDirector() {
+//        return director;
+//    }
+//
+//    public void setDirector(String director) {
+//        this.director = director;
+//    }
+//
+////    This is the constructor.
+//    public MovieWarmUp(String name, String director)throws IllegalArgumentException {
+//        if (name == null || name.isBlank() || director == null || director.isBlank()){
+//        throw new IllegalArgumentException(); }
+//        this.name = name;
+//        this.director = director;
+//    }
 //
 //public String audienceReaction(){
 //        return "oooooooooh" + name + "was so awesome";
 //}
 
-}
+//}
 
 
 // 4. Create a class called Movie. The class should define two instance variables of type String named name and director.
